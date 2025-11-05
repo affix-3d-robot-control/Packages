79cc4fe1e77b64d292299d601fd8928ea44bc001
Thijnmens <thijnsmulders04@gmail.com>
2025-11-05 10:37:59 +0100

    Add STLPackage

--- packages/__init__.py
+++ packages/__init__.py
@@ -1,5 +1,6 @@
 from .package import Package
 from .progress_package import ProgressPackage
+from .stl_package import STLPackage
 from .temperature_package import TemperaturePackage
 
 
@@ -12,5 +13,7 @@ def get_package(data: bytes) -> Package:
             return TemperaturePackage().to_package(data)
         case 0x02:
             return ProgressPackage().to_package(data)
+        case 0x03:
+            return STLPackage().to_package(data)
 
     raise ValueError(f"No package found for data with identifier {identifier}")
---
+++ packages/stl_package.py
@@ -0,0 +1,54 @@
+import struct
+import time
+
+from .package import Package
+
+
+class STLPackage(Package):
+    """A data package for transferring stl information."""
+
+    def __init__(self, timestamp: int = time.time(), stl: str = ""):
+        """Creates a stl package."""
+        super().__init__(0x03, "!IBLI")
+
+        self.timestamp = timestamp
+        self.stl = stl
+
+    def to_bytes(self) -> bytes:
+        """Converts the current package to a bytes object."""
+        package_format = self.format + f"{len(self.stl)}s"
+        stl_bytes = self.stl.encode("utf-8")
+
+        return struct.pack(
+            package_format,
+            struct.calcsize(package_format),
+            self.identifier,
+            int(self.timestamp),
+            len(stl_bytes),
+            stl_bytes
+        )
+
+    def to_package(self, data: bytes):
+        """Convert a bytes object into a STLPackage.
+
+        :param data: The data package
+        :return: The bytes object as a STLPackage
+        """
+        identifier = data[4]
+
+        # Check if identifier matches this package
+        if identifier != self.identifier:
+            raise ValueError(f"Package identifier for {__name__} "
+                             f"must be {self.identifier}. Found {identifier}")
+
+        package = struct.unpack(self.format, data[0:struct.calcsize(self.format)])
+
+        # Convert bytes to correct data
+        timestamp = package[2]
+        stl_size = package[3]
+        print(data)
+        print(package)
+
+        stl = struct.unpack(f"{stl_size}p", data[struct.calcsize(self.format):])
+
+        return STLPackage(timestamp=timestamp, stl=stl)
