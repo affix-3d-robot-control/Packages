17e416518765d0f3293080cfb5e5a9ce956a6934
Thijnmens <thijnsmulders04@gmail.com>
2025-11-21 15:21:09 +0100

    Change RobotDataPackage to match robot data files

--- packages/robot_data_package.py
+++ packages/robot_data_package.py
@@ -11,23 +11,26 @@ class RobotDataPackage(Package):
     def __init__(
             self,
             timestamp: int = time.time(),
-            name: str = "",
-            manufacturer: str = "",
-            min_acceleration: float = 0.0,
-            max_acceleration: float = 0.0,
-            min_speed: float = 0.0,
-            max_speed: float = 0.0
+            model: str = "",
+            brand: str = "",
+            axis: int = 0,
+            reach: int = 0,
+            payload: int = 0,
+            weight: int = 0,
+            accuracy: float = 0
+
     ):
         """Creates a robot package."""
-        super().__init__(0x05, "!IBLIIffff")
+        super().__init__(0x05, "!IBLIIIIIIf")
 
         self.timestamp = timestamp
-        self.name = name
-        self.manufacturer = manufacturer
-        self.min_acceleration = min_acceleration
-        self.max_acceleration = max_acceleration
-        self.min_speed = min_speed
-        self.max_speed = max_speed
+        self.model = model
+        self.brand = brand
+        self.axis = axis
+        self.reach = reach
+        self.payload = payload
+        self.weight = weight
+        self.accuracy = accuracy
 
     def from_robot(self, robot: Robot):
         """Converts a robot to RobotDataPackage."""
@@ -51,21 +54,22 @@ class RobotDataPackage(Package):
 
     def to_bytes(self) -> bytes:
         """Converts the current package to a bytes object."""
-        package_format = self.format + f"{len(self.name)}s" + f"{len(self.manufacturer)}s"
+        package_format = self.format + f"{len(self.model)}s" + f"{len(self.brand)}s"
 
         return struct.pack(
             package_format,
             struct.calcsize(package_format),
             self.identifier,
             int(self.timestamp),
-            len(self.name),
-            len(self.manufacturer),
-            self.min_acceleration,
-            self.max_acceleration,
-            self.min_speed,
-            self.max_speed,
-            self.name.encode("utf-8"),
-            self.manufacturer.encode("utf-8")
+            len(self.model),
+            len(self.brand),
+            self.axis,
+            self.reach,
+            self.payload,
+            self.weight,
+            self.accuracy,
+            self.model.encode("utf-8"),
+            self.brand.encode("utf-8")
         )
 
     def to_package(self, data: bytes):
@@ -87,28 +91,29 @@ class RobotDataPackage(Package):
 
         # Convert bytes to correct data
         timestamp = package[2]
-        name_size = package[3]
-        manufacturer_size = package[4]
+        model_size = package[3]
+        brand_size = package[4]
 
         name = struct.unpack(
-            f"{name_size}s",
+            f"{model_size}s",
             data[
-                header_size:header_size + name_size
+                header_size:header_size + model_size
             ])
 
         manufacturer = struct.unpack(
-            f"{manufacturer_size}s",
+            f"{brand_size}s",
             data[
-                header_size + name_size:
-                header_size + name_size + manufacturer_size
+                header_size + model_size:
+                header_size + model_size + brand_size
             ])
 
         return RobotDataPackage(
             timestamp=timestamp,
-            name=name[0].decode("utf-8"),
-            manufacturer=manufacturer[0].decode("utf-8"),
-            min_acceleration=package[5],
-            max_acceleration=package[6],
-            min_speed=package[7],
-            max_speed=package[8],
+            model=name[0].decode("utf-8"),
+            brand=manufacturer[0].decode("utf-8"),
+            axis=package[5],
+            reach=package[6],
+            payload=package[7],
+            weight=package[8],
+            accuracy=package[9]
         )
