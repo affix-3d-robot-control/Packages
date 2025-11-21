8ed5f26a1a7610dea77c26358ceaa5114aab17c6
Thijnmens <thijnsmulders04@gmail.com>
2025-11-21 15:42:06 +0100

    Changed parameters on robot class

--- packages/robot.py
+++ packages/robot.py
@@ -1,25 +1,23 @@
-from typing import NamedTuple, Tuple
-
-
-class _RobotAcceleration(NamedTuple):
-    min_acceleration: float
-    max_acceleration: float
-
-
-class _RobotSpeed(NamedTuple):
-    min_speed: float
-    max_speed: float
+from typing import Tuple
 
 
 class Robot:
     """Controls the robot."""
     name: str = ""
     code_file: str = ""
-    manufacturer: str = ""
-    acceleration: _RobotAcceleration
-    speed: _RobotSpeed
+    model: str = ""
+    brand: str = ""
+    axis: int = 0,
+    reach: int = 0,
+    payload: int = 0,
+    weight: int = 0,
+    acceleration: float = 0.0,
 
-    def move(self, location: Tuple[int, int, int], rotation: Tuple[float, float, float, float]):
+    def move(
+            self,
+            location: Tuple[int, int, int],
+            rotation: Tuple[float, float, float, float]
+    ):
         """Move the robot affector to the given location and rotation.
 
         :param location: The X, Y, Z location of the robot.
