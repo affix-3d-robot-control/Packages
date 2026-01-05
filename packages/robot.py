37bea1c3c2a20ace70d63d6e6ace4cc6f6aab4e0
Alexander Nyagolov <nyagolovaleksander@gmail.com>
2026-01-05 15:40:35 +0100

    added all functions

--- packages/robot.py
+++ packages/robot.py
@@ -18,14 +18,40 @@ class Robot:
         """Connects to the robot."""
         pass
 
-    def move(
+    def disconnect(self):
+        """Disconnects from the robot."""
+        pass
+
+    def abort_all(self):
+        """Aborts all programs running on a physical robot."""
+        pass
+
+    def curpos(self):
+        """Gets current position in pose of robot."""
+        pass
+
+    def curjpos(self):
+        """Gets current position in joints of robot."""
+        pass
+
+    def movePose(
             self,
             location: Tuple[int, int, int],
             rotation: Tuple[float, float, float, float]
     ):
-        """Move the robot affector to the given location and rotation.
+        """Move the robot affector to the given location and rotation via pose.
 
         :param location: The X, Y, Z location of the robot.
         :param rotation: The W, X, Y, Z rotation of the robot.
+        """
+        pass
+
+    def moveJoint(
+            self,
+            joints: Tuple[float, float, float, float, float]
+    ):
+        """Move the robot affector to the given location and rotation via joint.
+
+        :param joints: The values for each joint.
         """
         pass
